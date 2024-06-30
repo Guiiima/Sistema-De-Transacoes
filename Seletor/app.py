3b4afc3bceb3c92576c140b52e41b540e1d5be66
Gabriel <gabrieel.ferreira55@gmail.com>
2024-06-30 17:37:12 -0300

    Adição regras de banimento

--- Seletor/app.py
+++ Seletor/app.py
@@ -25,6 +25,7 @@ seletor_url = ip + ':5001'
 
 validacoes_pendentes = {}
 
+
 @dataclass
 class Validador(db.Model):
     id: int
@@ -35,6 +36,8 @@ class Validador(db.Model):
     hold: bool
     hold_expires: int
     ip: str
+    active: bool
+    expelled: int
 
     id = db.Column(db.Integer, primary_key=True)
     saldo = db.Column(db.Float)
@@ -44,10 +47,13 @@ class Validador(db.Model):
     hold = db.Column(db.Boolean)
     hold_expires = db.Column(db.Integer)
     ip = db.Column(db.String(30), nullable=False)
+    active = db.Column(db.Boolean)
+    expelled = db.Column(db.Integer)
 
 
 with app.app_context():
     db.create_all()
+
 
 @app.route('/cadastrar_seletor', methods=['POST'])
 def cadastrar_seletor():
@@ -55,6 +61,7 @@ def cadastrar_seletor():
     requests.post(url)
 
     return 200
+
 
 @app.route('/seletor/validador', methods=['POST'])
 def cadastrar_validador():
@@ -68,27 +75,46 @@ def cadastrar_validador():
 
     # Crie uma nova instância de Validador com os dados recebidos
     # saldo tem que ser maior que 50 para poder se cadastrar
-    if (data['saldo'] < 50):
-        return jsonify({"status": "error", "message": "É necessário ter no mínimo 50 moedas para se tornar um validador"}), 400
-    
-    novo_validador = Validador(
-        saldo=data['saldo'],
-        flags=0,
-        validacoes=0,
-        transacoes_corretas=0,
-        hold=False,
-        hold_expires=0,
-        ip=data['ip']
-    )
+    if data['saldo'] < 50:
+        return jsonify(
+            {"status": "error", "message": "É necessário ter no mínimo 50 moedas para se tornar um validador"}), 400
 
-    # Adicione e confirme a nova instância no banco de dados
-    try:
-        db.session.add(novo_validador)
-        db.session.commit()
-        return jsonify({"status": "success", "message": "Validador cadastrado com sucesso", "id": novo_validador.id}), 201
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({"status": "error", "message": f"Erro ao cadastrar validador: {str(e)}"}), 500
+    controle_validador_existente = False
+    validador_existente = Validador.query.filter_by(ip=data['ip']).first()
+    if validador_existente is not None:
+        if validador_existente.expelled > 2:
+            return jsonify({"status": "error", "message": "Validador banido permanentemente"}), 400
+        elif validador_existente.expelled > 0 and data['saldo'] >= 50 * 2 * validador_existente.expelled:
+            atualizar_validador(validador_existente.ip, data['saldo'])
+        else:
+            return jsonify({"status": "error",
+                            "message": f"Deve ter no mínimo {50 * 2 * validador_existente.expelled} para entrar novamente na rede"}), 400
+    else:
+        controle_validador_existente = True
+
+    if controle_validador_existente:
+        novo_validador = Validador(
+            ip=data['ip'],
+            saldo=data['saldo'],
+            flags=0,
+            validacoes=0,
+            transacoes_corretas=0,
+            hold=False,
+            hold_expires=0,
+            expelled=0,
+            active=True
+        )
+
+        # Adicione e confirme a nova instância no banco de dados
+        try:
+            db.session.add(novo_validador)
+            db.session.commit()
+            return jsonify(
+                {"status": "success", "message": "Validador cadastrado com sucesso", "id": novo_validador.id}), 201
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"status": "error", "message": f"Erro ao cadastrar validador: {str(e)}"}), 500
+
 
 @app.route('/remover_validador/<int:id>', methods=['DELETE'])
 def remover_validador():
@@ -104,6 +130,7 @@ def remover_validador():
     else:
         return jsonify(['Method Not Allowed'])
 
+
 @app.route('/transacoes/', methods=['POST'])
 def validar_transacoes():
     data = request.json
@@ -111,11 +138,13 @@ def validar_transacoes():
     sleep = 0
 
     # Escolher os validadores
-    validadores = Validador.query.filter(Validador.hold == False).all()
+    validadores = (Validador.query.filter(
+        Validador.hold == False,
+        Validador.active == True)
+                   .all())
 
     id_transacao = data['id']
     while len(selected_validadores) < 3:
-        print("Tentando")
         url = banco_url + '/cliente/' + str(data['remetente'])
         cliente = requests.get(url).json()
 
@@ -155,7 +184,7 @@ def validar_transacoes():
                 url = banco_url + f'/transacoes/{id_transacao}/2'
                 requests.post(url)
                 return jsonify(['Não haviam validadores suficientes']), 500
-            
+
             sleep += 1
             time.sleep(60)
 
@@ -164,7 +193,7 @@ def validar_transacoes():
     transacoes_response = requests.get(url_transacoes)
     transacoes_lista = transacoes_response.json()
 
-    ultima_transacao = sorted(transacoes_lista, key= lambda t: t['horario']).pop()
+    ultima_transacao = sorted(transacoes_lista, key=lambda t: t['horario']).pop()
 
     remetente_id = int(data['remetente'])
     transacoes_filtradas = [transacao for transacao in transacoes_lista if transacao['remetente'] == remetente_id]
@@ -172,11 +201,13 @@ def validar_transacoes():
     # Verificar transações com horário maior ou igual a data['horario'] - 1 minuto
     horario_limite = datetime.fromisoformat(data['horario']) - timedelta(minutes=1)
     for transacao in transacoes_filtradas:
-        transacao['horario'] = datetime.strptime(transacao['horario'], '%a, %d %b %Y %H:%M:%S %Z').strftime('%Y-%m-%dT%H:%M:%S.%f')
+        transacao['horario'] = datetime.strptime(transacao['horario'], '%a, %d %b %Y %H:%M:%S %Z').strftime(
+            '%Y-%m-%dT%H:%M:%S.%f')
     transacoes_recentes = [transacao for transacao in transacoes_filtradas if
                            datetime.fromisoformat(transacao['horario']) >= horario_limite]
-    
-    hora_atual = datetime.strptime(requests.get(banco_url + '/hora').json(), '%a, %d %b %Y %H:%M:%S %Z').strftime('%Y-%m-%dT%H:%M:%S.%f')
+
+    hora_atual = datetime.strptime(requests.get(banco_url + '/hora').json(), '%a, %d %b %Y %H:%M:%S %Z').strftime(
+        '%Y-%m-%dT%H:%M:%S.%f')
 
     # Exemplo de resposta (pode ser adaptado conforme necessário)
     conteudo_validacao = {
@@ -190,21 +221,21 @@ def validar_transacoes():
     }
 
     validadores = []
-    validacoes_pendentes[data['id']] = { 
+    validacoes_pendentes[data['id']] = {
         'transacao': {
             'remetente': data['remetente'],
             'recebedor': data['recebedor'],
             'saldo_remetente': cliente['qtdMoeda'],
             'valor': data['valor']
         },
-        'validadores': validadores, 
-        'respostas': 0, 
-        'n_validadores': len(selected_validadores) 
+        'validadores': validadores,
+        'respostas': 0,
+        'n_validadores': len(selected_validadores)
     }
 
     for valid in selected_validadores:
         url = 'http://' + valid.ip + '/validar_transacao/'
-        validador = { 'id': valid.id, 'status': 0 }
+        validador = {'id': valid.id, 'status': 0}
         validadores.append(validador)
         requests.post(url, json=conteudo_validacao)
 
@@ -216,6 +247,7 @@ def validar_transacoes():
         db.session.commit()
 
     return jsonify(['Transação em validação']), 200
+
 
 @app.route('/transacoes/resposta', methods=['POST'])
 def resposta_transacao():
@@ -229,18 +261,18 @@ def resposta_transacao():
 
     validador = next((v for v in transacao['validadores'] if v['id'] == id_validador), None)
 
-    if (not validador):
+    if not validador:
         return jsonify(['Validador não selecionado']), 400
-    
-    if (validador['status'] != 0):
+
+    if validador['status'] != 0:
         return jsonify(['Validador já havia respondido']), 400
 
     validador['status'] = status
     transacao['respostas'] += 1
 
-    if (transacao['respostas'] != transacao['n_validadores']):
+    if transacao['respostas'] != transacao['n_validadores']:
         return jsonify(['Validação Recebida']), 200
-    
+
     cont_status = Counter(validador['status'] for validador in transacao['validadores'])
     status_eleito, quant_status = cont_status.most_common(1)[0]
 
@@ -274,15 +306,15 @@ def resposta_transacao():
                 validador.flag = max(validador.flag - 1, 0)
 
             db.session.commit()
-        
+
         for valid in validadores_incorretos:
             validador = Validador.query.filter_by(id=valid['id']).first()
             validador.id = id
             validador.flags = validador.flags + 1
             validador.transacoes_corretas = 0
-            if validador.flags >= 2:
-                # TODO - Remover validador da rede, permitindo que ele retorne!!!
-                ...
+            if validador.flags > 2:
+                validador.active = False
+                validador.expelled += 1
 
             db.session.commit()
     else:
@@ -292,18 +324,18 @@ def resposta_transacao():
             validador.transacoes_corretas = validador.transacoes_corretas + 1
             if validador.transacoes_corretas >= 10000:
                 validador.flag = max(validador.flag - 1, 0)
-            
+
             db.session.commit()
-        
+
         for valid in validadores_incorretos:
             validador = Validador.query.filter_by(id=valid['id']).first()
             validador.id = id
             validador.flags = validador.flags + 1
             validador.transacoes_corretas = 0
-            if validador.flags >= 2:
-                # TODO - Remover validador da rede, permitindo que ele retorne!!!
-                ...
-            
+            if validador.flags > 2:
+                validador.active = False
+                validador.expelled += 1
+
             db.session.commit()
 
     # TODO - Aplicar taxa para o seletor ????
@@ -313,7 +345,27 @@ def resposta_transacao():
 
 def editar_cliente(id: int, moedas: int):
     url = banco_url + f'/cliente/{str(id)}/{str(moedas)}'
-    requests.post(url)   
+    requests.post(url)
 
 
-app.run(host='0.0.0.0', port= 5001, debug=True)
+@app.route('/validador/edit/<string:ip>/<float:saldo>', methods=['POST'])
+def atualizar_validador(ip, saldo):
+    if request.method == 'POST':
+        try:
+            data = request.json
+            validador = Validador.query.filter_by(ip=ip).first()
+            validador.saldo = saldo
+            validador.active = True
+            validador.flags = 0
+            db.session.commit()
+            return jsonify(['Alteração feita com sucesso'])
+        except Exception as e:
+            data = {
+                "message": "Atualização não realizada"
+            }
+            return jsonify(data)
+    else:
+        return jsonify(['Method Not Allowed'])
+
+
+app.run(host='0.0.0.0', port=5001, debug=True)
